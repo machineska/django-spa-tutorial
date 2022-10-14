2563905e5a80769bc300c9288d0d6ae4fad8eed2
machineska <alternative.xen@gmail.com>
2022-10-14 23:14:38 +0800

    add static file from django container

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from app.simple_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
